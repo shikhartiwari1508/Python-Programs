b34025047798ef0975a7a001dd8f9dd418ee8378
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-06 21:07:17 +0530

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+#Dictionary
+print("Dictionary :")
+dict = {"name":"shikhar", "age": 20, "eligible for vote": True}
+print(dict)
+
+# OR dictionary
+dict={
+    "name":"hello",
+    "roll no.": 25
+}
+print(dict["name"])
