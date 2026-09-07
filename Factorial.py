f3cdd6ccb8ecc17c9b908740b2b5252b7aab0891
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-07 22:40:31 +0530

    Factorial No. Program

--- Factorial.py
+++ Factorial.py
@@ -14,7 +14,7 @@ else :
 
 x= int(input("Enter the number = "))
 def factorial(x):
-    if(x==0) or (x==1):
+    if(x == 0) or (x == 1):
         return 1
     else:
         return x*factorial(x-1)
