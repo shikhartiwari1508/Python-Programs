5b9ac9672fef973f6611cd43b273adab79df0886
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-08 23:00:14 +0530

    Prime Number

--- prime.py
+++ prime.py
@@ -1,13 +1,13 @@
-num = int(input("Enter the number ="))
+num = int(input("Enter the Number ="))
 var = False
 if num == 0 or num == 1:
-    print(num , "is not a prime number")
+    print(num , "is not a Prime Number")
 elif num > 1:
     for i in range (2 , num):
         if (num % i) == 0:
             var = True
             break    
     if var:
-        print (num , "is not a prime number")
+        print (num , "is not a Prime Number")
     else:
-        print(num , "is a prime number")     
+        print(num , "is a Prime Number")     
