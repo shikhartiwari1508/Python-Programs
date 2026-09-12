151dcd61be2d9f7e6185ffbe9309564e65855311
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-12 22:42:18 +0530

    Python Libraries

--- libraries.py
+++ libraries.py
@@ -1,7 +1,7 @@
 # NUMPY PROGRAMME :
 
 import numpy as np
-arr=np.array([1,2,3])
+arr = np.array([1,2,3])
 print(arr)
 
 
@@ -20,7 +20,7 @@ print(df)
 #MATPLOTLIB PROGRAMME :
 
 import matplotlib.pyplot as plt
-x=[1,2,3]
-y=[4,5,6]
+x = [1,2,3]
+y = [4,5,6]
 plt.plot(x,y)
 plt.show()
