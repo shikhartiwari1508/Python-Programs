81bacf304b6b45726525e6145d0417d5d688bdb1
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-12 22:42:54 +0530

    Datasets

--- list.py
+++ list.py
@@ -1,30 +1,40 @@
 #List
+
 print("List :")
 list = [ 8,2,2.5,[-4,5],["apple","banana"]]
 print(list)
 print(list[1])
 
 
+
 #Tuple
+
 print("Tuple :")
 tuple = (("lion","dog","tiger"),("banana","apple","pineapple"))
 print(tuple)
 
 
+
 #Dictionary
+
 print("Dictionary :")
 dict = {"name":"shikhar", "age": 20, "eligible for vote": True}
 print(dict)
 
+
+
 # OR dictionary
-dict={
+
+dict = {
     "name":"hello",
     "roll no.": 25
 }
 print(dict["name"])
 
 
+
 #Set
+
 print("Set")
-s={1,2,3,4,5}
+s = {1,2,3,4,5}
 print(s)
