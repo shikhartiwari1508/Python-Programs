21b3b8e275da3cc441090842f0e1c6172c30fbdf
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-13 21:24:50 +0530

    Loops in Python

--- loops.py
+++ loops.py
@@ -33,24 +33,24 @@ for k in range (1,12,2):
 
 #While loop :
 
-'''i=0
+'''i = 0
 while (i<6):
     print(i)
-    i=i+1
+    i = i+1
 
-i=0
+i = 0
 while (i<=6):
     print(i)
-    i=i+1  '''
+    i = i+1  '''
 
 
-count=5
+count = 5
 while(count > 0):
      print(count)
      count = count - 1    
 
 
-count=5
+count = 5
 while(count > 0):
      print(count)
      count = count + 1         
