d69ee4e121d05f5bbf985597b85809e9252cdbe7
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-15 22:05:02 +0530

    QR creation

--- QR.py
+++ QR.py
@@ -3,15 +3,15 @@ import qrcode
 data = input("Enter text or URL: ")
 
 qr = qrcode.QRCode(
-    version=1,
-    box_size=10,
-    border=5
+    version = 1,
+    box_size = 10,
+    border = 5
 )
 
 qr.add_data(data)
-qr.make(fit=True)
+qr.make(fit = True)
 
-img = qr.make_image(fill_color="black", back_color="white")
+img = qr.make_image(fill_color = "black", back_color = "white")
 
 img.save("my_qr.png")
 
