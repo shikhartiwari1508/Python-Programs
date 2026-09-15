910f37d82f2d746f861e1b60596f15b1f87a69bf
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-15 22:05:40 +0530

    My First Python Project

--- project_1.py
+++ project_1.py
@@ -22,11 +22,11 @@ print(a)'''
 above 300 unit = 10 rs.'''
 
 units = int(input("Enter the electricity unit consumed :"))
-if units <=100:
+if units <= 100:
     bill = units*1.5
 elif units <=200:
     bill = (100*1.5)+((units - 100)*2.5)
-elif units <=200:
+elif units <= 200:
     bill = (100*1.5)+( 100*2.5) + ((units -200)*4)
 else:
     bill = (100*1.5)+(100*2.5)+(100*4)+((units-300)*10)
