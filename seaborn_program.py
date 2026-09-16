6e669e3f28a6e286519f1bc15da6387383d1f6df
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-16 22:33:13 +0530

    Seaborn Program

--- seaborn_program.py
+++ seaborn_program.py
@@ -15,45 +15,64 @@ df = pd.DataFrame({
     "Marks":[80, 58, 68, 95, 74, 55],
     #"Address":["Pratapgarh","Ballia","Kunda","Prayagraj","Allahabad","Rewa"]
 })
+
 # sns.barplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.scatterplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.lineplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.countplot(x="Name",data = df)
 # plt.show()
+
 # sns.boxplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.violinplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.histplot(df["Marks"])
 # plt.show()
+
 # sns.pairplot(df)
 # plt.show()
+
 # correlation = df.corr(numeric_only=True)
 # sns.heatmap(correlation, annot = True)
 # plt.show()
+
 # sns.barplot(data=df,x="Name",y="Marks", color="green")
 # plt.show()
+
 # sns.set_style("darkgrid")
 # sns.barplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.barplot(data=df,x="Name",y="Marks", hue= "Marks")
 # plt.show()
+
 # sns.kdeplot(df["Marks"])
 # plt.show()
+
 # sns.stripplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.swarmplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.catplot(data=df,x="Name",y="Marks",kind="bar")
 # plt.show()
+
 # plt.figure(figsize=(8,5))
 # sns.barplot(data=df,x="Name",y="Marks")
 # plt.show()
+
 # sns.barplot(data=df,x="Name",y="Marks")
 # plt.savefig("graph.png")
+
 sns.set_style("whitegrid")
 sns.barplot(data=df,x="Name",y="Marks",hue="Marks")
 plt.title("Student Marks")
