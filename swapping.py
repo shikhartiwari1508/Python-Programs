0bb5be457164affaf4e6711a107af0f59b132fc1
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-19 22:16:48 +0530

    Swapping Program

--- swapping.py
+++ swapping.py
@@ -1,6 +1,6 @@
-a=int(input("Enter the number :"))
-b=int(input("Enter the number :"))
-a,b=b,a
+a = int(input("Enter the number :"))
+b = int(input("Enter the number :"))
+a,b = b,a
 print("After Swapping")
-print("A=",a)
-print("B=",b)
+print("A = ",a)
+print("B = ",b)
