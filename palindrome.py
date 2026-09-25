3fb8f7795f070600e876e61841ee5f18cfe533ce
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-25 23:19:52 +0530

    Palindrome Number

--- palindrome.py
+++ palindrome.py
@@ -2,4 +2,21 @@ a = input("Enter the string :- ")
 if a == a[::-1]:
     print("Palindrome")
 else:
-    print("Not Palindrome")    
+    print("Not Palindrome")   
+
+
+
+
+# OR
+
+num = int(input("Enter the number :"))
+original =num
+reverse = 0
+while num > 0:
+    digit = num%10
+    reverse = reverse * 10 + digit
+    num = num // 10
+if (original == reverse):
+    print("Palindrome Number ") 
+else:
+    print("Not a Palindrome Number ")      
