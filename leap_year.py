e6ca31990b3909364bc6f75204f89a2fbc0e6b87
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-26 21:09:43 +0530

    Find Leap Year

---
+++ leap_year.py
@@ -0,0 +1,9 @@
+year = int(input("Enter the year :"))
+if year % 400 == 0:
+    print("Leap Year")
+elif year % 100 == 0:
+    print("Not a Leap Year") 
+elif year % 4 == 0:
+    print("Leap Year")
+else:
+    print("Not a Leap Year")           
