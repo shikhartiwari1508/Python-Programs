1930d12efedd56f4c19b67180ec77a2a11a7ff71
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-26 21:07:46 +0530

    List in Python

--- list.py
+++ list.py
@@ -26,6 +26,64 @@ for n in num :
 print("Smallest Number :", smallest)
 
 
+# Remove duplicates from a list
+
+numbers = [10,52,20,95,0,10,20]
+unique = []
+for num in numbers :
+    if num not in unique :
+        unique.append(num)
+print("Original list : ", numbers)
+print("List without duplicates :", unique)        
+
+
+# sum of elements in a list
+
+numbers = [10,20,30,40,50]
+sum = 0
+for num in numbers :
+    sum = sum + num
+print("Sum of list =", sum)
+
+
+#Count Even and odd in list
+
+numbers = [10,52,20,95,0,10,20]
+even = 0
+odd = 0 
+for num in numbers :
+    if num % 2 == 0 :
+        even = even + 1
+    else :
+        odd = odd + 1
+print("Even Numbers :", even)
+print("Odd Numbers : ", odd)            
+
+
+# find average of elements in a list
+
+numbers = [10,52,20,95,0,10,20]
+sum = 0
+for num in numbers :
+    sum = sum  + num
+    average = sum / len(numbers)
+    print("Average =", average)
+
+# Reverse a list 
+
+numbers = [10,20,30,40,50]
+reverse = numbers[ :: -1]
+print("Original List =", numbers)
+print("Reversed List =", reverse)
+
+#Check if an element exist in a list
+
+numbers = [10,52,20,95,0,10,20]
+num = int(input("Enter the Number :"))
+if num in numbers :
+    print("Element is Present in  a list")
+else:
+    print("Element is not present in a list")    
 
 #Tuple
 
